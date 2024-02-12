15f4c6ed5aae977f00c23c0785e123c6173bddcf
epatrezze <55457666+epatrezze@users.noreply.github.com>
2024-02-12 22:05:17 +0000

    ajuste comportamento dos itens

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,22 @@
 import React, { useState } from "react";
 import WebApp from "@twa-dev/sdk";
 import "./styles.css";
-import { Telegram } from "@twa-dev/types";
-import { BackButton } from "@twa-dev/sdk/react";
-import { MainButton } from "@twa-dev/sdk/react";
 
-<MainButton text="Finalizar" onClick={() => alert("submitted")} />;
+WebApp.MainButton.show();
+WebApp.MainButton.setText("Alloha");
+WebApp.MainButton.onClick(() => {
+  WebApp.showAlert("ALLOHA!!!");
+});
 
-<BackButton onClick={() => window.history.back()} />;
-
-declare global {
-  interface Window {
-    Telegram: Telegram;
-  }
+interface ItemProps {
+  nome: string;
+  imagemSrc: string;
+  preco: string;
 }
 
-window.Telegram.WebApp.HapticFeedback.notificationOccurred("success");
-
-// const data = "WebApp.initDataUnsafe";
-
-const App: React.FC = () => {
-  const [mostrarBotoes, setMostrarBotoes] = useState<boolean>(false);
-  const [contador, setContador] = useState<number>(0);
+const Item: React.FC<ItemProps> = ({ nome, imagemSrc, preco }) => {
+  const [mostrarBotoes, setMostrarBotoes] = useState(false);
+  const [contador, setContador] = useState(0);
 
   const mostrarOpcoes = () => {
     setMostrarBotoes(true);
@@ -52,6 +47,78 @@ const App: React.FC = () => {
   };
 
   return (
+    <div className="app-item">
+      {mostrarBotoes && <div className="app-item-counter">{contador}</div>}
+      <div className="app-item-photo">
+        <img src={imagemSrc} alt={nome} className="app-item-photo" />
+      </div>
+      <div className="cafe-item-label">
+        <span className="cafe-item-title">{nome}</span>
+        <span className="cafe-item-price">{preco}</span>
+      </div>
+      <div className="botoes-container">
+        {!mostrarBotoes && (
+          <button id="botao-add" className="botao-add" onClick={mostrarOpcoes}>
+            ADD
+          </button>
+        )}
+        {mostrarBotoes && (
+          <div className="botoes-opcoes">
+            <button className="botao-opcao-negativo" onClick={decrementar}>
+              -
+            </button>
+            <button className="botao-opcao-positivo" onClick={incrementar}>
+              +
+            </button>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+const App: React.FC = () => {
+  const itens = [
+    {
+      nome: "Purple Tulipe 40g",
+      imagemSrc:
+        "https://raw.githubusercontent.com/Tarikul-Islam-Anik/Telegram-Animated-Emojis/main/Animals%20and%20Nature/Tulip.webp",
+      preco: "$1.49",
+    },
+    {
+      nome: "Hibiscus 40g",
+      imagemSrc:
+        "https://raw.githubusercontent.com/Tarikul-Islam-Anik/Telegram-Animated-Emojis/main/Animals%20and%20Nature/Hibiscus.webp",
+      preco: "$1.49",
+    },
+    {
+      nome: "Camomila 40g",
+      imagemSrc:
+        "https://raw.githubusercontent.com/Tarikul-Islam-Anik/Telegram-Animated-Emojis/main/Animals%20and%20Nature/Herb.webp",
+      preco: "$1.49",
+    },
+    {
+      nome: "Lucky 40g",
+      imagemSrc:
+        "https://raw.githubusercontent.com/Tarikul-Islam-Anik/Telegram-Animated-Emojis/main/Animals%20and%20Nature/Four%20Leaf%20Clover.webp",
+      preco: "$1.49",
+    },
+    {
+      nome: "Snow 40g",
+      imagemSrc:
+        "https://raw.githubusercontent.com/Tarikul-Islam-Anik/Telegram-Animated-Emojis/main/Animals%20and%20Nature/Snowflake.webp",
+      preco: "$1.49",
+    },
+    {
+      nome: "Raio 40g",
+      imagemSrc:
+        "https://raw.githubusercontent.com/Tarikul-Islam-Anik/Telegram-Animated-Emojis/main/Animals%20and%20Nature/High%20Voltage.webp",
+      preco: "$1.49",
+    },
+    // Adicione mais itens conforme necessário
+  ];
+
+  return (
     <div className="App">
       <div className="logo-container">
         <img
@@ -62,228 +129,10 @@ const App: React.FC = () => {
         />
         <h1>Olá, {WebApp.initDataUnsafe?.user?.first_name}</h1>
       </div>
-      <div className="app-item">
-        {mostrarBotoes && <div className="app-item-counter">{contador}</div>}
-
-        <div className="app-item-photo">
-          <img
-            src="https://raw.githubusercontent.com/Tarikul-Islam-Anik/Telegram-Animated-Emojis/main/Animals%20and%20Nature/Tulip.webp"
-            alt="Tulip"
-            width="25"
-            height="25"
-            className="app-item-photo"
-          />
-        </div>
-        <div className="cafe-item-label">
-          <span className="cafe-item-title">Purple Tulipe 40g - </span>
-          <span className="cafe-item-price">$1.49</span>
-        </div>
-        <div className="botoes-container">
-          {!mostrarBotoes && (
-            <button
-              id="botao-add"
-              className="botao-add"
-              onClick={mostrarOpcoes}
-            >
-              ADD
-            </button>
-          )}
-          {mostrarBotoes && (
-            <div className="botoes-opcoes">
-              <button className="botao-opcao-negativo" onClick={decrementar}>
-                -
-              </button>
-              <button className="botao-opcao-positivo" onClick={incrementar}>
-                +
-              </button>
-            </div>
-          )}
-        </div>
-      </div>
-      <div className="app-item">
-        {mostrarBotoes && <div className="app-item-counter">{contador}</div>}
-        <div className="app-item-photo">
-          <img
-            src="https://raw.githubusercontent.com/Tarikul-Islam-Anik/Telegram-Animated-Emojis/main/Animals%20and%20Nature/Hibiscus.webp"
-            alt="Hibiscus"
-            width="25"
-            height="25"
-            className="app-item-photo"
-          />
-        </div>
-        <div className="cafe-item-label">
-          <span className="cafe-item-title">Hibiscus 40g - </span>
-          <span className="cafe-item-price">$1.49</span>
-        </div>
-        <div className="botoes-container">
-          {!mostrarBotoes && (
-            <button
-              id="botao-add"
-              className="botao-add"
-              onClick={mostrarOpcoes}
-            >
-              ADD
-            </button>
-          )}
-          {mostrarBotoes && (
-            <div className="botoes-opcoes">
-              <button className="botao-opcao-negativo" onClick={decrementar}>
-                -
-              </button>
-              <button className="botao-opcao-positivo" onClick={incrementar}>
-                +
-              </button>
-            </div>
-          )}
-        </div>
-      </div>
-      <div className="app-item">
-        {mostrarBotoes && <div className="app-item-counter">{contador}</div>}
-        <div className="app-item-photo">
-          <img
-            src="https://raw.githubusercontent.com/Tarikul-Islam-Anik/Telegram-Animated-Emojis/main/Animals%20and%20Nature/Herb.webp"
-            alt="Herb"
-            width="25"
-            height="25"
-            className="app-item-photo"
-          />
-        </div>
-        <div className="cafe-item-label">
-          <span className="cafe-item-title">Camomila 40g - </span>
-          <span className="cafe-item-price">$1.49</span>
-        </div>
-        <div className="botoes-container">
-          {!mostrarBotoes && (
-            <button
-              id="botao-add"
-              className="botao-add"
-              onClick={mostrarOpcoes}
-            >
-              ADD
-            </button>
-          )}
-          {mostrarBotoes && (
-            <div className="botoes-opcoes">
-              <button className="botao-opcao-negativo" onClick={decrementar}>
-                -
-              </button>
-              <button className="botao-opcao-positivo" onClick={incrementar}>
-                +
-              </button>
-            </div>
-          )}
-        </div>
-      </div>
-      <div className="app-item">
-        {mostrarBotoes && <div className="app-item-counter">{contador}</div>}
-        <div className="app-item-photo">
-          <img
-            src="https://raw.githubusercontent.com/Tarikul-Islam-Anik/Telegram-Animated-Emojis/main/Animals%20and%20Nature/Four%20Leaf%20Clover.webp"
-            alt="Four Leaf Clover"
-            width="25"
-            height="25"
-            className="app-item-photo"
-          />
-        </div>
-        <div className="cafe-item-label">
-          <span className="cafe-item-title">Lucky 40g - </span>
-          <span className="cafe-item-price">$1.49</span>
-        </div>
-        <div className="botoes-container">
-          {!mostrarBotoes && (
-            <button
-              id="botao-add"
-              className="botao-add"
-              onClick={mostrarOpcoes}
-            >
-              ADD
-            </button>
-          )}
-          {mostrarBotoes && (
-            <div className="botoes-opcoes">
-              <button className="botao-opcao-negativo" onClick={decrementar}>
-                -
-              </button>
-              <button className="botao-opcao-positivo" onClick={incrementar}>
-                +
-              </button>
-            </div>
-          )}
-        </div>
-      </div>
-      <div className="app-item">
-        {mostrarBotoes && <div className="app-item-counter">{contador}</div>}
-        <div className="app-item-photo">
-          <img
-            src="https://raw.githubusercontent.com/Tarikul-Islam-Anik/Telegram-Animated-Emojis/main/Animals%20and%20Nature/Snowflake.webp"
-            alt="Snowflake"
-            width="25"
-            height="25"
-            className="app-item-photo"
-          />
-        </div>
-        <div className="cafe-item-label">
-          <span className="cafe-item-title">Snow 40g - </span>
-          <span className="cafe-item-price">$1.49</span>
-        </div>
-        <div className="botoes-container">
-          {!mostrarBotoes && (
-            <button
-              id="botao-add"
-              className="botao-add"
-              onClick={mostrarOpcoes}
-            >
-              ADD
-            </button>
-          )}
-          {mostrarBotoes && (
-            <div className="botoes-opcoes">
-              <button className="botao-opcao-negativo" onClick={decrementar}>
-                -
-              </button>
-              <button className="botao-opcao-positivo" onClick={incrementar}>
-                +
-              </button>
-            </div>
-          )}
-        </div>
-      </div>
-      <div className="app-item">
-        {mostrarBotoes && <div className="app-item-counter">{contador}</div>}
-        <div className="app-item-photo">
-          <img
-            src="https://raw.githubusercontent.com/Tarikul-Islam-Anik/Telegram-Animated-Emojis/main/Animals%20and%20Nature/High%20Voltage.webp"
-            alt="High Voltage"
-            width="25"
-            height="25"
-            className="app-item-photo"
-          />
-        </div>
-        <div className="cafe-item-label">
-          <span className="cafe-item-title">Raio 40g - </span>
-          <span className="cafe-item-price">$1.49</span>
-        </div>
-        <div className="botoes-container">
-          {!mostrarBotoes && (
-            <button
-              id="botao-add"
-              className="botao-add"
-              onClick={mostrarOpcoes}
-            >
-              ADD
-            </button>
-          )}
-          {mostrarBotoes && (
-            <div className="botoes-opcoes">
-              <button className="botao-opcao-negativo" onClick={decrementar}>
-                -
-              </button>
-              <button className="botao-opcao-positivo" onClick={incrementar}>
-                +
-              </button>
-            </div>
-          )}
-        </div>
+      <div className="app-container">
+        {itens.map((item, index) => (
+          <Item key={index} {...item} />
+        ))}
       </div>
     </div>
   );
