6df5e9e047771d932ecb56ac53d5aa59854b38a5
epatrezze <55457666+epatrezze@users.noreply.github.com>
2024-02-12 00:01:05 +0000

    testzin

--- src/App.tsx
+++ src/App.tsx
@@ -7,10 +7,13 @@ WebApp.MainButton.onClick(() => {
   WebApp.showAlert("ALLOHA!!!");
 });
 
+const data = WebApp.initData;
+
 export default function App() {
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
+      {data && <h2>{data}</h2>}
       <h2>Start editing to see some magic happen!</h2>
     </div>
   );
