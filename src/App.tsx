950fc6eb4bceca5975b7e9b4994e7aba4796000f
epatrezze <55457666+epatrezze@users.noreply.github.com>
2024-02-12 00:08:17 +0000

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,22 @@
 import "./styles.css";
 import WebApp from "@twa-dev/sdk";
 
-WebApp.MainButton.show();
-WebApp.MainButton.setText("Alloha");
-WebApp.MainButton.onClick(() => {
-  WebApp.showAlert("ALLOHA!!!");
-});
+// WebApp.MainButton.show();
+// WebApp.MainButton.setText("Alloha");
+// WebApp.MainButton.onClick(() => {
+//   WebApp.showAlert("ALLOHA!!!");
+// });
 
 const data = WebApp.initData.toString();
 
 export default function App() {
   return (
     <div className="App">
+      <div id="twa-container">
+        <h1>Hello TWA</h1>
+      </div>
       <h1>Hello CodeSandbox</h1>
-      {data && <h2>{data}</h2>}
+      <h2>{data}</h2>
       <h2>Start editing to see some magic happen!</h2>
     </div>
   );
